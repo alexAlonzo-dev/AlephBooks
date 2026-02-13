b7ca1111e8036b2a9fb61f7643cedec97650aa64
alexAlonzo-dev <alejandroramirez.dev@outlook.com>
2026-02-12 20:38:23 -0600

    feat(ui): Implement react router

---
+++ aleph-books-ui/src/components/Cart.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Cart() {
+  return (
+    <>
+      <Header />
+      <div>CART</div>
+      <Footer />
+    </>
+  );
+}
+
+export default Cart;
