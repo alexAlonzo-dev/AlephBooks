c4724ee1cea5bbd3fada6875dd687b8d0a1a5768
alexAlonzo-dev <alejandroramirez.dev@outlook.com>
2026-02-16 22:33:14 -0600

    feat(ui): create cart and login page

--- aleph-books-ui/src/components/Cart.jsx
+++ aleph-books-ui/src/components/Cart.jsx
@@ -1,11 +1,36 @@
 import React from "react";
+import PageTitle from "./PageTitle";
+import { useNavigate } from "react-router-dom";
+import emptyCartImage from "../assets/util/emptycart.png";
 
-function Cart() {
+export default function Cart() {
+  const navigation = useNavigate();
+
+  const handleClick = () => {
+    navigation("/home", { state: { username: "madan" } });
+  };
+
   return (
-    <>
-      <div>CART</div>
-    </>
+    <div className="min-h-214 py-12 bg-normalbg dark:bg-darkbg font-primary">
+      <div className="max-w-4xl mx-auto px-4">
+        <PageTitle title="Your Cart" />
+        <div className="text-center text-gray-600 dark:text-lighter flex flex-col items-center">
+          <p className="max-w-xl px-2 mx-auto text-base mb-4">
+            Oops... Your cart is empty. Continue shopping
+          </p>
+          <img
+            src={emptyCartImage}
+            alt="Empty Cart"
+            className="max-w-75 mx-auto mb-6 dark:bg-light dark:rounded-md"
+          />
+          <button
+            onClick={handleClick}
+            className="py-2 px-4 bg-primary dark:bg-light text-white dark:text-black text-xl font-semibold rounded-sm flex justify-center items-center hover:cursor-pointer hover:bg-dark dark:hover:bg-dark dark:hover:text-light transition"
+          >
+            Back to Products
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
-
-export default Cart;
